4080076e18d1f6b44c83f606f341eac4120e7633
Michał Morawski <moras.technika@gmail.com>
2020-03-06 01:05:33 +0100

    Added movement, joystick controller

---
+++ Pitfall2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(1, rb.velocity.y);
+
+        if (Input.GetMouseButtonDown(0)) {
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+        }
+    }
+}
