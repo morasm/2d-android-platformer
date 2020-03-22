eba06c0790774b80d243a0c72cd88973be2fa192
Michał Morawski <moras.technika@gmail.com>
2020-03-22 03:16:30 +0100

    added assets

---
+++ Pitfall2020/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    private bool movingRight = true;
+
+    public void Move(float hMove){
+        if(hMove > 0 && !movingRight){
+            Flip();
+        }else if(hMove < 0 && movingRight){
+            Flip();
+        }
+    }
+
+    private void Flip() {
+        movingRight = !movingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+    }
+}
