b3c4a8ca8ff9104f70db570bbf93dcc9e17ab1bc
Michał Morawski <moras.technika@gmail.com>
2020-04-30 22:55:20 +0200

    MonsterController updated - movement added. Added MonsterSpawner to spawn monsters.

---
+++ Pitfall2020/Assets/Scripts/MonsterController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    private float hSpeed = 25.0f;
+
+    private float vSpeed = 15.0f;
+    private float verticalRange = 1.0f;
+    private float horizontalRange = 5.0f;
+
+    private bool movingRight;
+    private bool movingUp;
+
+
+    private float startX = -6.0f;
+
+    private float startY = -2.0f;
+
+    private Rigidbody2D m_Body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hSpeed > 0){
+            movingRight = true;
+        }else{
+            movingRight = false;
+        }
+
+        if (vSpeed > 0){
+            movingUp = true;
+        }else{
+            movingUp = false;
+        }
+
+        m_Body.velocity = new Vector2(hSpeed * Time.fixedDeltaTime * 10f,
+        vSpeed * Time.fixedDeltaTime * 5f);
+        if((m_Body.transform.position.x > startX + horizontalRange  && movingRight) 
+        ||(m_Body.transform.position.x < startX - horizontalRange  && !movingRight)){
+            		hSpeed *= -1;
+        }
+        if((m_Body.transform.position.y > startY + verticalRange  && movingUp) 
+        ||(m_Body.transform.position.y < startY - verticalRange  && !movingUp)){
+            		vSpeed *= -1;
+        }
+    }
+}
---
+++ Pitfall2020/Assets/Scripts/MonsterSpawnerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawnerScript : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+
+    private float[,] monsterLocations = new float[10,2]{
+        {1.0f,1.0f},
+        {-2.0f,-6.0f},
+        {2.0f,6.0f},
+        {-1.0f,-1.0f},
+        {3.0f,4.0f},
+        {12.0f,8.0f},
+        {11.0f,-4.0f},
+        {4.0f,8.0f},
+        {13.0f,11.0f},
+        {-12.0f, -9.0f}
+    };
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Spawn(){
+        for(int i = 0; i < monsterLocations.Length; i++){
+        GameObject monster = Instantiate(monsterPrefab) as GameObject;
+        monster.transform.position = new Vector2(monsterLocations[i,0],monsterLocations[i,1]);
+        }
+    }
+}
