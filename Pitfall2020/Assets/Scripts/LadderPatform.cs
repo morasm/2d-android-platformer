da7044359cf51f3d195702d073b5991bfd16655d
Michał Morawski <moras.technika@gmail.com>
2020-07-17 00:15:29 +0200

    hmmm

---
+++ Pitfall2020/Assets/Scripts/LadderPatform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderPatform : MonoBehaviour
+{
+    public PlatformEffector2D effector;
+    public float waitTime;
+    public Joystick joystick;
+    public PlayerController playerController;
+
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+
+    }
+
+    void Update()
+    {
+        if(joystick.Vertical > -0.20f ){
+            waitTime = 0.5f;
+            
+        }
+
+        if(joystick.Vertical < -0.20f && playerController.isClimbing){
+            if(waitTime <= 0){
+                effector.rotationalOffset = 180;
+                waitTime = 0.5f;
+            }else{
+                waitTime -= Time.deltaTime;
+            }
+        }else if(!playerController.isClimbing || joystick.Vertical > 0.2f){//joystick.Vertical > 0.2f
+            effector.rotationalOffset = 0;
+        }
+    
+    }
+}
--- Pitfall2020/Assets/Scripts/MonsterSpawnerScript.cs
+++ Pitfall2020/Assets/Scripts/MonsterSpawnerScript.cs
@@ -24,7 +24,7 @@ public class MonsterSpawnerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Spawn();
+        Spawn();
     }
 
     // Update is called once per frame
